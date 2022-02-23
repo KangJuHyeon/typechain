fc171b9295200e4604462062f3c9f262c3facdb2
KangJuHyeon <alinos@naver.com>
2022-02-23 17:40:02 +0900

    Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
+class Human {
+    public name: string;
+    private age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    } // 메서드인데 클래스가 실행될 때마다 호출된다.
+}
 interface Human {
     name: string;
     age: number;
     gender: string;
 }
 
-const person = {
-    name: "Choonsik",
-    age: 26,
-    gender: "male"
-};
+const lynn = new Human("Lynn", 18, "female");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
